72b35316a0d9dc6905c36223186ca37f40159a4f
Mamuche <marionlaroche17@gmail.com>
2025-05-21 10:21:08 +0200

    fix while

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -15,8 +15,8 @@ int wildcmp(char *s1, char *s2)
 
     if (*s2 == '*')
     {
-        while (*(s2 + 1) == '*')
-            s2++;
+        if (*(s2 + 1) == '*')
+            return (wildcmp(s1, s2 + 1));
 
         return (wildcmp(s1, s2 + 1) || (*s1 && wildcmp(s1 + 1, s2)));
     }
