575f7a9992dc06c35a2df678ed39e36805dbb865
Mamuche <marionlaroche17@gmail.com>
2025-05-21 10:26:49 +0200

    betty documentation

--- wild_cmp/0-wildcmp.c
+++ wild_cmp/0-wildcmp.c
@@ -5,6 +5,8 @@
  *
  * @s1: first string to compare
  * @s2: second string to compare
+ * Return: 1 if the strings can be considered
+ * identical, otherwise return 0
  */
 int wildcmp(char *s1, char *s2)
 {
