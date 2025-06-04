456e0cd31c2412961a9ed92ac607fa5e479599c1
Mamuche <marionlaroche17@gmail.com>
2025-06-04 13:44:06 +0200

    betty

--- regex/regex.c
+++ regex/regex.c
@@ -1,7 +1,7 @@
 /**
  * regex_match - checks whether a given pattern
  * matches a given string
- * 
+ *
  * @str: string to scan
  * @pattern: regular expression
  * Return: 1 if the strings can be considered
@@ -10,18 +10,16 @@
 int regex_match(char const *str, char const *pattern)
 {
 	if (*pattern == '\0')
-    	return (*str == '\0');
+		return (*str == '\0');
 
 	if (*str == '.' || *str == '*')
-    return (0);
+    	return (0);
 
 	int first_match = (*str && (*pattern == *str || *pattern == '.'));
 
 	if (*(pattern + 1) == '*')
 	{
-		return (
-        regex_match(str, pattern + 2) || //j'ignore
-        (first_match && regex_match(str + 1, pattern)));//je rajoute un char
+		return (regex_match(str, pattern + 2) || (first_match && regex_match(str + 1, pattern)));
 	}
 
 	return (first_match && regex_match(str + 1, pattern + 1));
